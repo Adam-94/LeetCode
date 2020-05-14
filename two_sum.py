25b2757f30a4641f181c15ef53c0e469ca65825a
Adam Hughes <ashughes07@aol.com>
2020-05-14 18:30:06 +0100

    initial commit

---
+++ two_sum.py
@@ -0,0 +1,9 @@
+def two_sum(nums, target):
+      for i, num in enumerate(nums):
+        if target-num in nums and i!=nums.index(target-num):
+            return [nums.index(target-num), i]
+
+if __name__ == '__main__':
+    nums = [3,2,3]
+    target = 6
+    print(two_sum(nums, target))
